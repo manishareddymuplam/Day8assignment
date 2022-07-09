523fc49a9c509f66875738cac738545d9d8ef103
manishareddymuplam <manishamuplam@gmail.com>
2022-07-09 16:26:42 +0530

    added:uc3

--- bridgelabz/day8/Employee.java
+++ bridgelabz/day8/Employee.java
@@ -1,20 +1,35 @@
 package com.bridgelabz.day8;
 
-public class Employee {
-	private static final int WAGE_PER_HOUR = 20;
+public class Employee 
+{
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    private static final int HALF_DAY_HOUR = 5;
     private static final int FULL_DAY_HOUR = 10;
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee wage Computation Problem");
         int dailyWage;
         int empStatus = (int) (Math.random() * 10) % 2;
-        if(empStatus==1) {
+        int empStatus1 = (int) (Math.random() * 10) % 3;
+        if(empStatus1==1) {
             System.out.println("Employee present");
             //UC2 calculate Daily wage
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("daily wage : "+dailyWage);
         }
         else{
+         if (empStatus1==2) {
+            System.out.println("Employee present");
+            //UC3 part time calculate Daily wage
+            dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+            System.out.println("part time daily wage : "+dailyWage);
+        } else{
             System.out.println("Employee Absent");
         }
     }
+    }
 }
+	
+    
