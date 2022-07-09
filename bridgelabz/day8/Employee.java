01aabfb36f7a56f2524957555cb673b9a8242f3a
manishareddymuplam <manishamuplam@gmail.com>
2022-07-09 16:35:30 +0530

    added:uc4

--- bridgelabz/day8/Employee.java
+++ bridgelabz/day8/Employee.java
@@ -1,35 +1,48 @@
 package com.bridgelabz.day8;
 
-public class Employee 
-{
-    private static final int WAGE_PER_HOUR = 20;
-    private static final int IS_FULL_TIME = 1;
-    private static final int IS_PART_TIME = 2;
-    private static final int HALF_DAY_HOUR = 5;
-    private static final int FULL_DAY_HOUR = 10;
-    public static void main(String[] args) {
+public class Employee {
+	 private static final int WAGE_PER_HOUR = 20;
+	    private static final int IS_FULL_TIME = 1;
+	    private static final int IS_PART_TIME = 2;
+	    private static final int HALF_DAY_HOUR = 5;
+	    private static final int FULL_DAY_HOUR = 10;
+	    public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee wage Computation Problem");
-        int dailyWage;
-        int empStatus = (int) (Math.random() * 10) % 2;
-        int empStatus1 = (int) (Math.random() * 10) % 3;
-        if(empStatus1==1) {
-            System.out.println("Employee present");
-            //UC2 calculate Daily wage
-            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("daily wage : "+dailyWage);
-        }
-        else{
-         if (empStatus1==2) {
-            System.out.println("Employee present");
-            //UC3 part time calculate Daily wage
-            dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
-            System.out.println("part time daily wage : "+dailyWage);
-        } else{
-            System.out.println("Employee Absent");
-        }
-    }
-    }
+	        System.out.println("Welcome to Employee wage Computation Problem");
+	        int dailyWage;
+	        int dailyWage1 = 0;
+	        int empStatus = (int) (Math.random() * 10) % 3;
+	        if(empStatus==1) {
+	            System.out.println("Employee present");
+	            //UC2 calculate Daily wage
+	            dailyWage1 = WAGE_PER_HOUR * FULL_DAY_HOUR;
+	            System.out.println("daily wage : "+dailyWage1);
+	        }
+	        else if (empStatus==2) {
+	            System.out.println("Employee present");
+	            //UC3 part time calculate Daily wage
+	            dailyWage1 = WAGE_PER_HOUR * HALF_DAY_HOUR;
+	            System.out.println("part time daily wage : "+dailyWage1);
+	        } else{
+	            System.out.println("Employee Absent");
+	        //UC4 Switch statement
+	        switch (empStatus) {
+	            case IS_FULL_TIME:
+	                System.out.println("Employee worked FULL TIME");
+	                dailyWage1 = WAGE_PER_HOUR * FULL_DAY_HOUR;
+	                System.out.println("dailywage : "+dailyWage1);
+	                break;
+	            case IS_PART_TIME:
+	                System.out.println("Employee worked PART_TIME");
+	                dailyWage1 = WAGE_PER_HOUR * HALF_DAY_HOUR;
+	                System.out.println("dailywage : "+dailyWage1);
+	                break;
+	            default:
+	                System.out.println("Employee is Absent");
+	        }
+	    }
+	}	
 }
-	
+
+
     
