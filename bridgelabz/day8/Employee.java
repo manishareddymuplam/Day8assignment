62e608fcbd16e738f18adc0715c4d067865f246b
manishareddymuplam <manishamuplam@gmail.com>
2022-07-09 16:13:32 +0530

    added:uc2

--- bridgelabz/day8/Employee.java
+++ bridgelabz/day8/Employee.java
@@ -1,11 +1,17 @@
 package com.bridgelabz.day8;
 
 public class Employee {
-	public static void main(String[] args) {
+	private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOUR = 10;
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee wage Computation Problem");
+        int dailyWage;
         int empStatus = (int) (Math.random() * 10) % 2;
         if(empStatus==1) {
             System.out.println("Employee present");
+            //UC2 calculate Daily wage
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("daily wage : "+dailyWage);
         }
         else{
             System.out.println("Employee Absent");
